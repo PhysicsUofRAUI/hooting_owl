3e683bf0ed821ed73d7dd19fa979add5fc3a17b1
Kody Rogers <kodyrogers@pop-os.localdomain>
2023-06-02 21:40:16 -0600

    Add simple arduino prox. buzzer example

---
+++ simple_arduino_prox_buzzer/simple_arduino_prox_buzzer.ino
@@ -0,0 +1,44 @@
+
+
+/*
+  Really short Arduino Program that buzzers a piezo buzzer
+  when something is within 51 cm.
+
+  It was edited from the tutorial described below:
+
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+
+*/
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+const int buzzer = 11;
+// defines variables
+long duration;
+int distance;
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  if (duration < 3000)
+  {
+    tone(buzzer, 350, 15);
+  }
+}
+
+
